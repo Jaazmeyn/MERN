ba83861a914a719a811113c8ea2a5475bb10c061
jaazmeyn <jasmin.blanda@gmail.com>
2021-05-21 17:56:44 +0200

    Css transition group

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,11 +1,18 @@
 //mobile
 import React from 'react'; 
 import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    const content = <aside className="side-drawer">{props.children}</aside>
+    const content = <CSSTransition 
+        in={props.show} 
+        timeout={200} 
+        classNames="slide-in-left" 
+        mountOnEnter 
+        unmountOnExit>
+            <aside className="side-drawer">{props.children}</aside></CSSTransition>
 
     //which content render where
     return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
