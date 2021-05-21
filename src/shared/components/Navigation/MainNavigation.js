b5e519f569643d714a43cc407e87162828b304c1
jaazmeyn <jasmin.blanda@gmail.com>
2021-05-21 17:29:26 +0200

    add backdropclick 2 close menue

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop'
 
 import './MainNavigation.css';
 
@@ -16,11 +17,13 @@ const MainNavigation = props => {
         setDrawerIsOpen(true);
     };
 
-
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
     return (
         <React.Fragment>
-
-            {drawerIsOpen && (<SideDrawer>
+            { drawerIsOpen && <Backdrop onClick = { closeDrawer } />}
+            { drawerIsOpen && (<SideDrawer>
                 <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
